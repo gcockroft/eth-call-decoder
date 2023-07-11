799f5a740bdad7b7c014a260a416bf7c452b0456
Gareth Cockroft <gcockroft534@gmail.com>
2023-07-11 09:15:04 -0700

    Writes to file, support for multiple input files at a time

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # eth_call_decoder
-Take csv of eth_call responses and decode readable data
+A tool for bulk decoding the function signature of `eth_call` requests through hexcode signature lookups in the [Ethereum Signature Database](https://www.4byte.directory/). 
+
+## Usage
+The tool parses out the `data` field from a raw CSV file containing the JSON of *all* `eth_call` request data. Example input line:
+```javascript
+{
+    ""method"":""eth_call"",
+    ""params"": [
+        {
+            ""to"":""0x00000000000000000..."",
+            ""data"":""0x70a08231000000000...""
+        },
+        ""latest""
+    ],
+    ""id"":43,
+    ""jsonrpc"":
+    ""2.0""
+}
+```
+
+1. Put raw data files in `/raw_data`.
+2. Provide the filename(s) to decode as command line arguments, 1 or more files.
+3. Decoded CSV files will appear in `/decoded_data` as `decoded_<RAW_FILENAME>` in the format "hex_signature,function_signature".
+
+**Notes:**
+- Data is parsed out as the first 10 characters succeeding the raw string `""data"":""`.
+- The decoder sleeps repeatedly to avoid 429 errors, increasing runtime.
--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
 import requests
+import sys
 import time
 
-def main():
-    # Open file. 
-    f = open("../raw_data/Test API Logs_2023-07-06_23_15_00_2023-07-07_00_15_00_filters-2_by_request.csv", "r")
+def decode(filename):
+    fr = open("../raw_data/" + filename, "r")
+    fw = open("../decoded_data/decoded_" + filename, "w")
+    fw.write('hex_signature,text_signature\n')
 
     # Read past header line.
-    print(f.readline())
-    i = 0
+    fr.readline()
+
     while True:
-        # Get data
-        line = f.readline()
+        # Get hexcode to decode.
+        line = fr.readline()
         if not line:
             break
         hexcode = line.split('""data"":""')[1][:10]
-        i += 1 
+
         # Get function signature with API
         url =  'https://www.4byte.directory/api/v1/signatures/?hex_signature=' + hexcode
         try:
@@ -24,16 +26,34 @@ def main():
                 time.sleep(int(response.headers['Retry-After']))
                 response = requests.get(url)
 
+            # Handle and write response
             json = response.json()
+            text_signature = json['results'][0]['text_signature']
+            fw.write(hexcode + ',' + text_signature + '\n')
             print(json['results'][0]['text_signature'])
             response.raise_for_status()
-        except requests.exceptions.HTTPError as errh:
+
+        except requests.exceptions.HTTPError as err:
             if response.status_code == 404:
                 print("404 Hex signature not found")
             else:
-                print(str(errh))
-        except requests.exceptions.C:
-            print("Unknown error")
+                print(str(err))
+        except requests.exceptions.ConnectionError as err:
+            print("Connection error.")
+        except Exception as err:
+            print(f"Unknown error occured {err=}, {type(err)=}")
+
+    return
+
+def main():
+    # Handle no filename given.
+    if len(sys.argv) == 1:
+        print("Please provide filename(s) as command line arguments.")
+        return
+    print(str(sys.argv)) 
+    for i in range(1,len(sys.argv)):
+        decode(sys.argv[1])
+    return 
 
 if __name__ == "__main__":
     main()
